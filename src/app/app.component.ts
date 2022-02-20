756f3d09fd13ea56f27dce17377efafa54e2c51d
Akshay-IndieDev <smartscorpius.sting918@gmail.com>
2022-02-21 04:53:16 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Pipe, PipeTransform } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import {
   FormGroup,
   FormBuilder,
@@ -9,7 +10,6 @@ import {
   NgForm,
   Validators,
 } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
 
 interface USERS {
   id: Number;
@@ -17,13 +17,6 @@ interface USERS {
   last_name: String;
   email: String;
   gender: String;
-}
-
-export interface USERSTITLE {
-  id: Number;
-  first_name: String;
-  last_name: String;
-  email: String;
 }
 
 @Component({
@@ -34,6 +27,9 @@ export interface USERSTITLE {
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   value = '';
+  filterValues: any = {};
+  isMale = false;
+  isFemale = false;
 
   handleInput(event: Event) {
     this.value = (event.target as HTMLInputElement).value;
@@ -69,7 +65,7 @@ export class AppComponent {
     });
 
     this.dataSource.filterPredicate = function (data, filter: string): boolean {
-      var dataa = Users_Title.filter((element) => element.id == data.id);
+      var dataa = Users.filter((element) => element.id == data.id);
       return (
         data.email.toString().toLowerCase().includes(filter) ||
         dataa[0].first_name
@@ -79,12 +75,50 @@ export class AppComponent {
         dataa[0].last_name
           .toString()
           .toLowerCase()
-          .includes(filter.toLowerCase())
+          .includes(filter.toLowerCase()) ||
+        data.gender.toString().toLowerCase() === filter
       );
     };
   }
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  isMaleChecked(value) {
+    const filterValue = value ? 'Male' : null;
+    this.isMale = value;
+    console.log('Male: ' + this.isMale);
+    this.ifBothChecked()
+      ? this.resetFilters()
+      : this.applyGenderFilter(filterValue);
+  }
+
+  isFemaleChecked(value) {
+    const filterValue = value ? 'Female' : null;
+    this.isFemale = value;
+    console.log('Female: ' + this.isFemale);
+    this.ifBothChecked()
+      ? this.resetFilters()
+      : this.applyGenderFilter(filterValue);
+  }
+
+  resetFilters() {
+    this.dataSource.filter = '';
+  }
+
+  ifBothChecked(): boolean {
+    //Identify which checkbox is checked
+    if (this.isMale && this.isFemale) return true;
+    else return false;
+  }
+
+  applyGenderFilter(value) {
+    if (null != value) this.dataSource.filter = value.trim().toLowerCase();
+    else this.resetFilters();
+  }
+
+  onChange(){
+    this.isMale = !this.isMale;
   }
 }
 
@@ -147,63 +181,17 @@ const Users: USERS[] = [
   },
 ];
 
-const Users_Title: USERSTITLE[] = [
-  {
-    id: 1,
-    first_name: 'Jeanette',
-    last_name: 'Penddreth',
-    email: 'jpenddreth0@census.gov',
-  },
-  {
-    id: 2,
-    first_name: 'Giavani',
-    last_name: 'Frediani',
-    email: 'gfrediani1@senate.gov',
-  },
-  {
-    id: 3,
-    first_name: 'Noell',
-    last_name: 'Bea',
-    email: 'nbea2@imageshack.us',
-  },
-  {
-    id: 4,
-    first_name: 'Willard',
-    last_name: 'Valek',
-    email: 'wvalek3@vk.com',
-  },
-  {
-    id: 5,
-    first_name: 'Thomas',
-    last_name: 'Burke',
-    email: 'tbburke@census.gov',
-  },
-  {
-    id: 6,
-    first_name: 'Stephie',
-    last_name: 'Simpson',
-    email: 'ssimpson@senate.gov',
-  },
-  {
-    id: 7,
-    first_name: 'Natalie',
-    last_name: 'Evans',
-    email: 'nevans98@imageshack.us',
-  },
-  {
-    id: 8,
-    first_name: 'William',
-    last_name: 'Zhang',
-    email: 'wzhang3@vk.com',
-  },
-];
+class GENDER {
+  gender: String;
+  isSelected: Boolean;
+}
 
 @Pipe({
   name: 'FilterData',
 })
 export class OrdinalPipe implements PipeTransform {
   transform(value: Number): String {
-    var data = Users_Title.filter((user) => user.id === value);
+    var data = Users.filter((user) => user.id === value);
     return data[0].first_name;
   }
 }
